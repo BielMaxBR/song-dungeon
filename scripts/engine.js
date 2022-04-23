e552a789a0750b5b3d6029603647d51b56894769
Gabriel Maximo <gbielmaximo@gmail.com>
2022-04-23 18:45:55 +0000

    gambiarra nivel senior confirmada pela nasa

--- scripts/engine.js
+++ scripts/engine.js
@@ -40,12 +40,11 @@ export default class Engine {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
         this.ctx.fillStyle = "white"
         this.ctx.font = `normal ${this.fontSize}px monospace`
-        for (let y = 1; y < this.size.height+1; y++) {
-            let row = ""
-            for (let x = 1; x < this.size.width+1; x++) {
-                row += (y%2 == 0)-x%4 == 0 ? "█" : "#"
+        for (let y = 0; y < this.size.height; y++) {
+            for (let x = 0; x < this.size.width; x++) {
+                const char = this.map[[x,y]]?.char || "_"//(y%2 == 0)-x%2 == 0 ? "." : "@"
+                this.ctx.fillText(char, (this.fontSize/4)+(this.fontSize/1.818*x), (this.fontSize)*(y+1))
             }
-            this.ctx.fillText(row, this.fontSize/4, (this.fontSize)*y)
             
         }
     }
